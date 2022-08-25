a3dede3f33b6b888ebba39f04d91e57daca2dd40
drumnistnakano <syxqh349ysykn@gmail.com>
2022-08-26 00:13:56 +0900

    abc081 B 回答

--- abc081/B/src/main.rs
+++ abc081/B/src/main.rs
@@ -3,11 +3,21 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        arr: [usize; n],
+        arr: [i32; n],
     }
 
-    println!("{}", arr[0]);
-    for m in arr.iter() {
-        println!("{:?}", m);
+    let mut counter = 0;
+    let mut _arr = arr;
+
+    'countup: loop {
+        for m in _arr.iter() {
+            if m % 2 != 0 {
+                break 'countup;
+            }
+        }
+        counter += 1;
+        _arr = _arr.iter().map(|x| x/2).collect();
     }
+
+    println!("{}", counter);
 }
